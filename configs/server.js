20132a8bffed5f2098768f93270a7554acaf9db1
oskcarsv <oskcarqsv@gmail.com>
2024-05-12 22:05:19 -0600

    server running

---
+++ configs/server.js
@@ -0,0 +1,45 @@
+'use strict'
+
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import bcryptjs from 'bcryptjs';
+import { dbConnection } from './mongo.js';
+
+
+class Server{
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+        // this.usuarioPath = '/products-storage/v1/user';
+
+        this.middlewares();
+        this.connectDB();
+        this.routes();
+    }
+
+    async connectDB() {
+        await dbConnection();
+    }
+
+    middlewares(){
+        this.app.use(express.urlencoded({extended: false}));
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(helmet());
+        this.app.use(morgan('dev'));
+    }
+
+    routes() {
+        // this.app.use(this.usuarioPath, userRoutes);
+    }
+
+    listen(){
+        this.app.listen(this.port, () => {
+            console.log('Server running on port ', this.port);
+        });
+    }
+}
+
+export default Server;
